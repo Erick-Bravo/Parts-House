7a9ed592f6c56b12c6c5867f7e2eb41fb55d425e
Erick-Bravo <erickbrvo@gmail.com>
2021-02-04 12:57:32 -0800

    leftNavbar component started bedore react-icons

---
+++ frontend/src/components/UserMainPage/index.js
@@ -0,0 +1,24 @@
+import React from "react"
+import "./index.css"
+
+
+
+
+
+
+
+
+const UserMainPage = () => {
+    return (
+        <div id="user-main-page">
+            <div>
+                <p>Icon</p>
+            </div>
+            <div id="mascot">
+                <img src="https://i.ibb.co/Sx3THPm/vector-creator-1500-1by1.png" alt="vector-creator-1500-1by1" border="0" height="55px"/>
+            </div>
+        </div>
+    )
+}
+
+export default UserMainPage
