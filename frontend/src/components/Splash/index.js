1db9132806a84f2cf8cee4a7f4ecafe32eb7d83a
Erick-Bravo <erickbrvo@gmail.com>
2021-02-06 13:45:09 -0800

    css for splash resized. still need sign-up to be resized

--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -23,8 +23,8 @@ const Splash = () => {
         <> 
             <div id="splash-page">
                 <div id="splash-logo">
-                    <img src="https://i.ibb.co/Sx3THPm/vector-creator-1500-1by1.png" alt="vector-creator-1500-1by1" border="0" height="200px"/>
-                    <h2>Parts House</h2>
+                    <img src="https://i.ibb.co/Sx3THPm/vector-creator-1500-1by1.png" alt="vector-creator-1500-1by1" border="0" height="400px"/>
+                    <p>Parts House</p>
                 </div>
                 <div id="splash-button-container">
                     <LoginFormModal />
