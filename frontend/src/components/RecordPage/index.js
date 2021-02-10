c7b5f9520f89c8fec4977f0a697ac060642d4e0a
Erick-Bravo <erickbrvo@gmail.com>
2021-02-10 10:03:36 -0800

    parts now on the record page, needs css

--- frontend/src/components/RecordPage/index.js
+++ frontend/src/components/RecordPage/index.js
@@ -1,9 +1,30 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { useSelector, useDispatch } from "react-redux"
 import { useParams } from "react-router-dom"
 import { fetchUserRecord } from "../../store/records"
 
 import "./index.css"
+
+
+
+const Part = ({part}) => {
+
+    return (
+        <div id="parts-container">
+            <h2>Parts</h2>
+            <div id="left">
+                <div id="name">{part.name}</div>
+                <div id="make">{part.make}</div>
+            </div>
+            <div id="right">
+                <div>Model --- {part.model}</div>
+                <div>Serial --- {part.serial}</div>
+            </div>
+        </div>
+    )
+}
+
+
 
 const RecordPage = () => {
 
@@ -11,27 +32,45 @@ const RecordPage = () => {
     const { recordId } = useParams()
     const numRecordId = parseInt(recordId)
 
-
+    const [ parts, setParts ] = useState([])
 
     useEffect(() => {
         dispatch(fetchUserRecord(numRecordId))
     }, [dispatch, numRecordId])
 
-    const record = useSelector(state => state)
+    const record = useSelector(state => state.record)
 
-    // console.log(record)
+    useEffect(() => {
+        if (record.length === 0) {
+            return
+        }
+        const p = record.Parts
+        setParts(p)
+    }, [record, recordId]);
+
 
     return (
         <div id="user-main-page">
-            
+
             <div id="display-box">
-                <p>This is the Record Page</p>
+                <div id="record-container">
+                    <div id="record-name">{record.name}</div>
+                    <div id="record-make">{record.make}</div>
+                    <h2>Info</h2>
+                    <p>Model - {record.model}</p>
+                    <p>Serial - {record.serial}</p>
+                    <p>Cost - ${record.cost}</p>
+                    
+                </div>
+                    {parts.map(part => {
+                        return <Part part={part} key={part.id}/>
+                    })}
             </div>
 
             <div id="mascot">
-                <img src="https://i.ibb.co/Sx3THPm/vector-creator-1500-1by1.png" alt="vector-creator-1500-1by1" border="0" height="100px"/>
+                <img src="https://i.ibb.co/Sx3THPm/vector-creator-1500-1by1.png" alt="vector-creator-1500-1by1" border="0" height="100px" />
             </div>
-            
+
         </div>
     );
 }
