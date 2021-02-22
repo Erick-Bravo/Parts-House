86535df58ec5e1d00a584049b9dff38d4e81bb2f
Erick-Bravo <erickbrvo@gmail.com>
2021-02-22 00:13:36 -0800

    footer done with working links

--- frontend/src/components/zFooter/index.js
+++ frontend/src/components/zFooter/index.js
@@ -3,18 +3,37 @@ import "./index.css"
 
 
 const Footer = () => {
+
+
+    const linkedIn = () => {
+        window.location.href = 'https://www.linkedin.com/in/erick-bravo-448234203/';
+    };
+
+    const gitHub = () => {
+        window.location.href = 'https://github.com/Erick-Bravo';
+    };
+
+    const angelList = () => {
+        window.location.href = 'https://www.linkedin.com/in/erick-bravo-448234203/';
+    };
+
+    
+
+
+
+
     return (
         <div id="footer">
 
-            <button>
+            <button onClick={linkedIn}>
                 <img alt="none" src="https://www.iconsdb.com/icons/preview/white/linkedin-3-xxl.png" height="70px" />
             </button>
 
-            <button>
+            <button onClick={gitHub}>
                 <img alt="none" src="https://www.sferalabs.cc/wp-content/uploads/github-logo-white-300x199.png" height="80px" />
             </button>
 
-            <button>
+            <button onClick={angelList}>
                 <img alt="none" src="https://angel.co/assets/shared/peace/white_large-7f08f00fe68dacf8c1900f162d5e426a21ddd42f807d857a461b792562f040ff.png" height="80px" background-color="white" />
             </button>
 
