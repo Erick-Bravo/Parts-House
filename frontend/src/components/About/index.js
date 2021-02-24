12dd505698eb2e5b1ac9ab3978cb02daab768397
Erick-Bravo <erickbrvo@gmail.com>
2021-02-23 23:30:34 -0800

    meta tags added for linkedin profile

--- frontend/src/components/About/index.js
+++ frontend/src/components/About/index.js
@@ -25,9 +25,11 @@ const About = () => {
                     <h2>About</h2>
                     <ul>
                         <li>Parts House is your very own customizable parts tracker.</li>
-                        <li>You can record valuable model/serial info from home or office appliances and/or electronics.</li>
-                        <li>You can even record where to re-purchase these items when you need them most.</li>
+                        <li>Record valuable model/serial info from home or office appliances and/or electronics.</li>
+                        <li>You can even record where to re-purchase these items, or parts for these items when you need them most.</li>
+                        <li>For example, save a record for your tv remote and save the type of batteries along with the link of where to re-purchase them.</li>
                         <li>Parts House is your very own customizable parts-house!</li>
+                        <li>Tech used: Node.js, Express, PostgreSQL, JavaScript, React, Redux </li>
                     </ul>
                     <button onClick={goBack}>Back</button>
                 </div>
