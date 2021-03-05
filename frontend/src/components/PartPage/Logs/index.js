ce2b527ae2c94136d33a2fb1d8427826a25e8ceb
Erick-Bravo <erickbrvo@gmail.com>
2021-03-04 23:40:54 -0800

    log component created. css working

---
+++ frontend/src/components/PartPage/Logs/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Logs = () => {
+    return (
+        <>
+            <p>last log</p>
+            <p>Feb 17th 2021</p>
+            <p>Note: lorem ipsum</p>
+        </>
+    );
+};
+
+export default Logs
