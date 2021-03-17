176e77edf2e714dc7de80d117055a9af69aa65e5
Erick-Bravo <erickbrvo@gmail.com>
2021-03-16 21:33:29 -0700

    log component updated with state but still not correct format for date-fns

--- frontend/src/components/PartPage/Logs/index.js
+++ frontend/src/components/PartPage/Logs/index.js
@@ -1,8 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import LogsFormModal from "../LogsFormModal"
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAllLogs } from "../../../store/logs";
+import { format } from "date-fns"
 
 
 const Logs = () => {
@@ -11,19 +12,32 @@ const Logs = () => {
     const { partId } = useParams();
     const numPartId = parseInt(partId);
 
+    const [Log, setLog] = useState("")
+
     useEffect(() => {
         dispatch(fetchAllLogs(numPartId))
-    }, [dispatch])
+    }, [dispatch, numPartId])
 
     const logs = useSelector(state => state.logs)
-    console.log(logs)
+    const lastLog = logs[logs.length - 1]
+    console.log(lastLog)
+    
+    useEffect(() => {
+        if(logs.length === 0) {
+            return
+        } else {
+            const lastLog = logs[logs.length - 1]
+            setLog(lastLog)
+            console.log(`INSIDE USEEFFECT: ${lastLog}`)
+        }
+    }, [numPartId, logs, lastLog]);
 
-
+ /* {lastLog && <p>{format(lastLog.date, "PPP")}</p>} */
     return (
         <>
             <h2>Last Log</h2>
-            <p>Feb 17th 2021</p>
-            <p>Note: lorem ipsum</p>
+            {Log && <p>{format(Log.date, "PP")}</p>}
+            <p>Note: {Log.note ? Log.note : "none"}</p>
             <LogsFormModal />
         </>
     );
