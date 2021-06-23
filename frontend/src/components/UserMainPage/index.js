5326064b0f7668d3431acecd7c543d2f1ef79396
Erick-Bravo <erickbrvo@gmail.com>
2021-06-23 16:44:01 -0700

    message to recruiter

--- frontend/src/components/UserMainPage/index.js
+++ frontend/src/components/UserMainPage/index.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 
 import "./index.css";
@@ -9,19 +10,22 @@ import "./index.css";
 
 const EmptyDisplayPage = () => {
 
+    
+
 
     return (
         <div id="user-main-page">
-            <div>
-                
-            </div>
             
             
             <div id="display-box">
-                <p>Hello! Having a good day?</p>
+                <p>Welcome! Having a good day?</p>
+                <p>- The Left Navbar 'houses' the different locations a user gets to store</p>
+                <p>- Each location has their own assets and parts associated with those assets</p>
+                <p>- Feel free to poke around!</p>
             </div>
 
             <div id="mascot">
+                <p>{`Old goofy logo that only you get to see  =====>`}</p>
                 <img src="https://i.ibb.co/Sx3THPm/vector-creator-1500-1by1.png" alt="vector-creator-1500-1by1" border="0" height="100px"/>
             </div>
             
